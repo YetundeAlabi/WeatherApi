339e7c23e26b4f565b0417ea6f1c318bee390003
YetundeAlabi <alabiyetunde007@gmail.com>
2023-07-31 18:37:46 +0100

    removed db.sqlite3

--- locationapi/settings.py
+++ locationapi/settings.py
@@ -86,13 +86,12 @@ CSRF_TRUSTED_ORIGINS = ["https://weatherapi-xlqh.onrender.com"]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
