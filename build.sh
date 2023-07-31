00e908594bf669a71d670faebee3cd9e641e6285
YetundeAlabi <alabiyetunde007@gmail.com>
2023-07-31 18:07:20 +0100

    added migrations

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python makemigrations
+python manage.py makemigrations
 python manage.py migrate
